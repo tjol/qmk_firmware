c8f21702c7f3345ad44dd592f0f111bf17fc54f2
Thomas Jollans <tjol@tjol.eu>
2024-07-02 21:34:18 +0200

    maybe a tapping toggle of 3 is safer

--- keyboards/lily58/keymaps/tjol/config.h
+++ keyboards/lily58/keymaps/tjol/config.h
@@ -29,7 +29,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define QUICK_TAP_TERM 0
 #define TAPPING_TERM 200
 
-#define TAPPING_TOGGLE 2
+#define TAPPING_TOGGLE 3
 
 #undef RGBLIGHT_LED_COUNT
 #define RGBLIGHT_EFFECT_BREATHING
